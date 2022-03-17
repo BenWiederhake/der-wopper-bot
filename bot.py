46035d8076f197a4396bca773c2159ba69f2a80c
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2022-03-17 19:11:30 +0100

    Connect dispatcher with new command

--- bot.py
+++ bot.py
@@ -229,6 +229,7 @@ def run():
     dispatcher.add_handler(CommandHandler("do_w", cmd_for('do_w')))
     dispatcher.add_handler(CommandHandler("do_p", cmd_for('do_p')))
     dispatcher.add_handler(CommandHandler("choose", cmd_for('choose')))
+    dispatcher.add_handler(CommandHandler("whytho", cmd_for('whytho')))
     dispatcher.add_handler(CommandHandler("uptime", cmd_for('uptime')))
     dispatcher.add_handler(CommandHandler("players", cmd_for('players')))
 
